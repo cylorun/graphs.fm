56e0da3c787905fca1cda5dd7f3090b113bdba23
cylorun <allidabbi@gmail.com>
2025-03-04 00:22:36 +0000

    in the beninging

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+import express, {Request, Response} from 'express';
+const app = express();
+
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import * as path from "path";
+
+
+const PORT = Number(process.env.PORT) || 7000;
+const ENVIRONMENT = process.env.ENVIRONMENT || "dev";
+
+if (ENVIRONMENT === "production") {
+    app.set('trust proxy', 1); // to correctly identify IPs
+}
+
+app.use(cookieParser());
+
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'src', 'views', 'pages'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => { // not found page
+    res.status(404).json({message: "welcome"})
+});
+
+app.use((req: Request, res: Response) => { // not found page
+     res.status(404).json({error: "not found"})
+});
+
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught exception:");
+    console.error("Error name: " + err.name);
+    console.error("Error message: " + err.message);
+    console.error("Stack trace:\n" + err.stack);
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`http://localhost:${PORT}`);
+});
