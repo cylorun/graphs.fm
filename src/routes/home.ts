6db08607a0e5d96ee995fc032456c139727efa00
cylorun <allidabbi@gmail.com>
2025-03-06 15:49:40 +0000

    fix ejs errors

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -4,9 +4,12 @@ import {getCurrentlyPlaying} from "../services/spotifyService";
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
-   const loggedIn = req.session?.uid;
+   const loggedIn = !!req.session?.uid;
+   const id = parseInt(req.params.id);
 
-   res.render("index", {loggedIn});
+   const isYou = id === req.session?.uid; // if it's your profile
+
+   res.render("index", {loggedIn, isYou, userId: req.session?.uid});
 });
 
 export default router;
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -9,7 +9,10 @@ router.get('/', (req: Request, res: Response) => {
         return;
     }
 
-    res.render("login", {loggedIn});
+    const id = parseInt(req.params.id);
+    const isYou = id === req.session?.uid; // if it's your profile
+
+    res.render("login", {loggedIn, isYou, userId: req.session?.uid});
 });
 
 export default router;
