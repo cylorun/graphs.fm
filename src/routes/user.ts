24151436082577918c6b12980ed751126d883087
cylorun <allidabbi@gmail.com>
2025-03-06 23:35:37 +0000

    display played at for user page

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,7 @@
 import {Request, Response, Router} from "express";
 import {getCurrentlyPlaying} from "../services/spotifyService";
 import {getRecentTracks} from '../services/trackService';
+import moment from "moment";
 
 
 const router = Router();
@@ -20,8 +21,11 @@ router.get('/:id', async (req: Request, res: Response) => {
     const id = parseInt(req.params.id);
 
     const isYou = id === req.session?.uid; //if it's your profile
+    const recentTracks = (await getRecentTracks(id)).map(track => {
+        const playedAt = moment(track.playedAt).fromNow();
+        return { ...track, playedAt }
+    });
 
-    const recentTracks = await getRecentTracks(id);
     const currentTrack = await getCurrentlyPlaying(id);
 
     res.render("user", {recentTracks, currentTrack, loggedIn, isYou, userId: req.session?.uid});
