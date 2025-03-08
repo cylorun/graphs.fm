2d7c03f9929c825c062e3ae00e94b151cc1ed18b
cylorun <allidabbi@gmail.com>
2025-03-08 23:16:50 +0000

    remove console.dir

--- src/routes/global.ts
+++ src/routes/global.ts
@@ -14,7 +14,6 @@ router.get('/', async (req: Request, res: Response) => {
         const isYou = id === req.session?.uid; // if it's your profile
 
         const topTracks = await getTopMostListenedTracks()
-        console.dir(topTracks, {depth: null})
         const topArtists = await getTopArtists();
 
         res.render("global", {loggedIn, isYou, userId: req.session?.uid, topTracks, topArtists});
