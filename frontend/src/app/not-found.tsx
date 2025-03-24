b033359361177f56af5c6621c63121f65c3c06ab
cylorun <allidabbi@gmail.com>
2025-03-24 22:30:46 +0000

    404 page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Container from "@/components/container";
+import React from "react";
+
+
+const NotFound: React.FC = () => {
+    return (
+        <>
+            {/*<Hero></Hero>*/}
+            <Container className="relative flex items-center justify-center min-h-screen pb-0 pt-32 md:pt-40 px-5">
+                The page your are looking for doesn't exist
+            </Container>
+
+        </>
+    )
+}
+
+
+export default NotFound
