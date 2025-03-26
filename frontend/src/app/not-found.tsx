adab8c63829b718433e2d023376bf12a6021d029
cylorun <allidabbi@gmail.com>
2025-03-26 22:32:59 +0000

    fix build

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFound: React.FC = () => {
         <>
             {/*<Hero></Hero>*/}
             <Container className="relative flex items-center justify-center min-h-screen pb-0 pt-32 md:pt-40 px-5">
-                The page your are looking for doesn't exist
+                The page your are looking for doesn&apos;t exist
             </Container>
 
         </>
