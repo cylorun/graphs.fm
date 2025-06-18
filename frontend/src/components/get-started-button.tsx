a9850542e74bbc7c25440bcf38ac6acf99c03b90
cylorun <allidabbi@gmail.com>
2025-06-18 23:17:58 +0000

    [web] change getstarted-button to a tag instead of Link

--- frontend/src/components/get-started-button.tsx
+++ frontend/src/components/get-started-button.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 const GetStartedButton: React.FC = () => {
-
     return (
-        <Link href={`${API_URL}/auth/spotify/login`} className="text-black bg-primary hover:bg-primary-accent px-8 py-3 rounded-full transition-colors">
+        <a
+            href={`${API_URL}/auth/spotify/login`}
+            className="text-black bg-primary hover:bg-primary-accent px-8 py-3 rounded-full transition-colors"
+        >
             Get started
-        </Link>
-    )
-}
+        </a>
+    );
+};
 
 export default GetStartedButton
