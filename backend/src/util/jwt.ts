04850000399dd06d1915556f6118ad59a2206e76
cylorun <allidabbi@gmail.com>
2025-06-18 23:50:14 +0000

    [sys] nginx config changes

--- backend/src/util/jwt.ts
+++ backend/src/util/jwt.ts
@@ -18,7 +18,8 @@ export const saveTokenAsCookie = (res: Response, token: string) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: NODE_ENV === 'production',
-        sameSite: "none",
+        sameSite: "strict",
+        path: '/',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 };
